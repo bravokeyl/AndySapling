a26b6a7d950d72432f78cf1f5369219aa56e1a80
bravokeyl <bravokeyl@gmail.com>
2017-03-10 01:27:11 +0530

    resource grid

--- page_blog.php
+++ page_blog.php
@@ -3,7 +3,7 @@
 * Template Name: Blog
 */
 add_action('genesis_after_header','genesis_do_subnav');
-remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+remove_action( 'genesis_entry_header', 'genesis_post_info', 8 );
 remove_action( 'genesis_entry_content', 'genesis_do_post_image', 8 );
 remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
 
