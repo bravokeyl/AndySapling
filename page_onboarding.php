b7dc53f0cfe7e43557f10ec342bcc216ae530dcd
bravokeyl <bravokeyl@gmail.com>
2017-03-11 20:18:33 +0530

    onboarding

--- page_onboarding.php
+++ page_onboarding.php
@@ -6,7 +6,7 @@ Template Name: Onboarding
 //* Force full-width-content layout
 add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 
-add_action( 'genesis_after_header', 'bk_product_page_widgets' );
+add_action( 'genesis_after_header', 'bk_onboarding_page_widgets' );
 //* Remove the default Genesis loop
 // remove_action( 'genesis_loop', 'genesis_do_loop' );
 //* Remove .site-inner
@@ -16,15 +16,15 @@ add_filter( 'genesis_markup_content', '__return_null' );
 add_theme_support( 'genesis-structural-wraps', array( 'header', 'footer-widgets', 'footer' ) );
 
 //* Add full-width body class to the head
-add_filter( 'body_class', 'showcase_add_body_class' );
-function showcase_add_body_class( $classes ) {
+add_filter( 'body_class', 'bk_add_bclass' );
+function bk_add_bclass( $classes ) {
 
 	$classes[] = 'full-width on-boading-page';
 	return $classes;
 
 }
 
-function bk_product_page_widgets(){
+function bk_onboarding_page_widgets(){
   echo '<h2 class="screen-reader-text">' . __( 'Main Content', 'showcase' ) . '</h2>';
 
   genesis_widget_area( 'on-boarding-page-1', array(
@@ -35,14 +35,15 @@ function bk_product_page_widgets(){
     'before' => '<div id="on-boarding-page-2" class="on-boarding-page-2 front-page-2 flexible-widget-area"><div class="wrap"><div class="flexible-widgets widget-area">',
     'after'  => '</div></div></div>',
   ) );
-  genesis_widget_area( 'on-boarding-page-3', array(
-    'before' => '<div id="on-boarding-page-3" class="on-boarding-page-3 front-page-2 flexible-widget-area"><div class="wrap"><div class="flexible-widgets widget-area">',
-    'after'  => '</div></div></div>',
-  ) );
-  genesis_widget_area( 'on-boarding-page-4', array(
-    'before' => '<div id="on-boarding-page-4" class="on-boarding-page-4 front-page-2 flexible-widget-area"><div class="wrap"><div class="flexible-widgets widget-area">',
-    'after'  => '</div></div></div>',
-  ) );
 }
-//* Run the Genesis loop
+function bk_bottom_page_widgets() {
+	 genesis_widget_area( 'on-boarding-page-3', array(
+     'before' => '<div id="on-boarding-page-3" class="on-boarding-page-3 front-page-2 flexible-widget-area"><div class="wrap"><div class="flexible-widgets widget-area">',
+     'after'  => '</div></div></div>',
+   ) );
+   genesis_widget_area( 'on-boarding-page-4', array(
+     'before' => '<div id="on-boarding-page-4" class="on-boarding-page-4 front-page-2 flexible-widget-area"><div class="wrap"><div class="flexible-widgets widget-area">',
+     'after'  => '</div></div></div>',
+   ) );
+}
 genesis();
